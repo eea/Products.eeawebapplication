6852d32167135cf3cde7926246338eff3d29f76a
tiberich <tiberich@24821446-e7ea-0310-81ac-b189f4507b0b>
2011-11-18 16:15:25 +0000

    Tagged Products.eeawebapplication 4.1.

---
+++ trunk/Products.eeawebapplication/tags/4.1/Products/eeawebapplication/interface.py
@@ -0,0 +1,8 @@
+""" Interfaces
+"""
+import zope.interface
+
+class IEEAWebApplication(zope.interface.Interface):
+    """ marker interface for ATFolders which should have customized
+        main_template
+    """
