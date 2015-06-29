27291931f6fba9d29e1e259284641c49b510bf72
razvanchitu <razvanch@users.noreply.github.com>
2015-06-29 13:13:06 +0000

    Fixed pylint issues

--- Products/eeawebapplication/browser/__init__.py
+++ Products/eeawebapplication/browser/__init__.py
@@ -1,13 +1,13 @@
 ################################################################################
 # Copyright (C) 2006  EEA - European Enviromental Agency
-# 			    Antonio De Marinis <antonio.de.marinis@eea.eu.int> 
+# 			    Antonio De Marinis <antonio.de.marinis@eea.eu.int>
 #                    Sasha Vincic <sasha.vincic@lovelysystems.com>
-#                    
+#
 # This program is free software; you can redistribute it and/or
 # modify it under the terms of the GNU General Public License
 # as published by the Free Software Foundation; either version 2
 # of the License, or (at your option) any later version.
-# 
+#
 # This program is distributed in the hope that it will be useful,
 # but WITHOUT ANY WARRANTY; without even the implied warranty of
 # MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
